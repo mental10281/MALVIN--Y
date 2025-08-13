7beb22d63957194704885e58614aafea98b054a5
mental10281 <mentalmunda652@gmail.com>
2025-08-13 11:49:46 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~9ukTwSAB#NP_1jq0rhgObp6EX4oe8o5NFERIa-dvX-UE8W0trDYw",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "923177278405", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
